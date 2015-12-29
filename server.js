f75567681bc6c1d69ac661d7418cbbdc6bdbb1e3
Jon Kuperman <jon.kuperman@gmail.com>
2015-12-29 17:08:40 -0500

    basic get requests

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
+var unirest = require('unirest');
 var app = express();
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/kelly', function (req, res) {
+  unirest.get("https://ismaelc-pinterest.p.mashape.com/deviantdiy/boards")
+  .header("X-Mashape-Key", "LrCc1gyiCTmshSvy6HHx3HcuBeolp1ftf2ojsnmqoUt7UkPTop")
+  .header("Accept", "application/json")
+  .end(function (result) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(result.body.body));
+  });
 });
 
 var server = app.listen(3000, function () {
